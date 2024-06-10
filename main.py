193837c4327d5525b112c11e2932956d3a492f03
willgpennell <wpennell48@gmail.com>
2024-06-10 13:59:13 -0230

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+from streamlit.source_util import _on_pages_changed, get_pages
+import json
+
+
+from pathlib import Path
+
+import yaml
+from yaml.loader import SafeLoader
+
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+authenticator.login()
+
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
+
+
+
+
+
+    
