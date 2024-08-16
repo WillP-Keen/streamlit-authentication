283b4db17d01d5e5f33316c35e44e696bd3e7a2c
willgpennell <wpennell48@gmail.com>
2024-08-16 13:44:09 -0230

    added streamlit secrets

--- main.py
+++ main.py
@@ -2,38 +2,64 @@ import streamlit as st
 import streamlit_authenticator as stauth
 from streamlit.source_util import _on_pages_changed, get_pages
 import json
-
+from pymongo import MongoClient
 
 from pathlib import Path
 
 import yaml
 from yaml.loader import SafeLoader
 
-with open('./config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+client = MongoClient(st.secrets["MONGODB_CONNECTION_STRING"])
+
+db = client["will"]
+users_collection = db["UserAuthentication"]
 
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['pre-authorized']
-)
-
-authenticator.login()
-
-if st.session_state["authentication_status"]:
-    authenticator.logout()
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
+def get_users(collection):
+    users = {}
+    for user in collection.find():
+        users[user["user"]] = {"name": user["user"], "password": user["password"]}
+    return users
 
 
+def login_user(username, password):
+    user = users_collection.find_one({"user": username})
+    if user and user["password"] == password:
+        return True
+    else:
+        return False
 
 
+def get_credentials():
+    credentials = {"usernames": {}}
+    for user in users_collection.find():
+        credentials["usernames"][user["user"]] = {
+            "name": user["user"],
+            "password": user["password"],
+        }
+    return credentials
 
-    
+
+def authenticate():
+    credentials = get_credentials()
+
+    authenticator = stauth.Authenticate(
+        credentials=credentials,
+        cookie_name="mykeen-auth-cookie",
+        cookie_key="mykeen123321neekym",
+        cookie_expiry_days=10,
+    )
+
+    authenticator.login()
+
+    if st.session_state["authentication_status"]:
+        st.success("welcome")
+
+    elif st.session_state["authentication_status"] is False:
+        st.error("Username/Password Incorrect")
+
+    elif st.session_state["authentication_status"] is None:
+        st.warning("Please enter username and password")
+
+
+authenticate()
